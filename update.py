98e58265485afd0cffe571a986b375a5184754c8
hibryid <32869232+hibryid@users.noreply.github.com>
2020-03-09 03:59:19 +0200

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,21 @@
 import os, sys, subprocess
+import requests
 from time import sleep
 
-pid2 = os.getpid()
-print(pid2)
+
+print("started new script")
+
+scriptname = os.path.basename(__file__)
+
+url = "https://raw.githubusercontent.com/hibryid/Testupdate/master/update.py"
+r = requests.get(url)
+with open(scriptname, 'wb') as f:
+    f.write(r.content)
+    print("Done downloading")
+
+
+while True:
+    print("sleeping enough")
+    sleep(15)
+    print(os.path.basename(__file__))
+    os.execl(sys.executable, sys.executable, * sys.argv)
